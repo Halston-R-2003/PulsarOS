e212656da19cabeccc8af277a9edfc4f603eb596
Halston Robbins <halstonr2003@gmail.com>
2024-05-11 09:53:01 -0700

    IRQ

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <gdt.h>
 #include <idt.h>
 #include <isrs.h>
+#include <irq.h>
 
 int32_t kernel_main(void)
 {
@@ -11,10 +12,11 @@ int32_t kernel_main(void)
 	gdt_init();
 	idt_init();
 	isrs_init();
+	irq_init();
 
 	// Display OS Name & Version as well as Test VGA Colors
 	vga_set_text_color(0xB, 0x1);
-	vga_puts("PulsarOS v0.0.0.0007\n");
+	vga_puts("PulsarOS v0.0.0.0008\n");
 
 	for (int32_t i = 0; i < 16; ++i)
 	{

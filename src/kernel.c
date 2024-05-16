97f5bc25c80e9ab62273039407588fc6a0c7a08f
Halston Robbins <halstonr2003@gmail.com>
2024-05-16 01:02:16 -0700

    kprintf

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,7 @@
 #include <irq.h>
 #include <timer.h>
 #include <keyboard.h>
+#include <kprintf.h>
 
 int32_t kernel_main(void)
 {
@@ -21,7 +22,7 @@ int32_t kernel_main(void)
 
 	// Display OS Name & Version as well as Test VGA Colors
 	vga_set_text_color(0xB, 0x1);
-	vga_puts("PulsarOS v0.0.0.0011\n");
+	vga_puts("PulsarOS v0.0.0.0012\n");
 
 	for (int32_t i = 0; i < 16; ++i)
 	{
@@ -31,6 +32,17 @@ int32_t kernel_main(void)
 	vga_puts("\n");
 
 	vga_set_text_color(0xF, 0x0);
+
+	kprintf("Testing kprintf...\n");
+
+	int8_t* test_str = "QWERTY";
+	kprintf("%s\n", test_str);
+
+	int32_t test_num = 5000;
+	kprintf("%d\n", test_num);
+
+	int32_t test_hex = 0xB00755AA;
+	kprintf("%x\n", test_hex);
 
 	for (;;);
 	return 0;
